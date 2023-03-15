8289dd86360cb3c7f8135d073eb60904cbc7bae3
Ayush3524 <127313182+Ayush3524@users.noreply.github.com>
2023-03-15 18:09:35 +0530

    Update Array2D.java

--- Array2D.java
+++ Array2D.java
@@ -1,9 +1,9 @@
 class Array2D{
 	public static void main(String[] args) {
-		int matrix[][]={                        {1,2,3},
-							{4,5,6},
-							{7,8,9},
-							{10,11,12}
+		int matrix[][]={ {1,2,3},
+				 {4,5,6},
+				 {7,8,9},
+				 {10,11,12}
 						};
 		System.out.println("\nThe Matrix is: ");
 		for (int i=0;i<4 ;i++ ) {
