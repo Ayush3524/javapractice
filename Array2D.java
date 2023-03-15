69932829b14ae61473e28637b5af876e4fdf22da
Ayush3524 <127313182+Ayush3524@users.noreply.github.com>
2023-03-15 09:44:25 +0530

    Update Array2D.java

--- Array2D.java
+++ Array2D.java
@@ -1,6 +1,6 @@
 class Array2D{
 	public static void main(String[] args) {
-		int matrix[][]={    {1,2,3},
+		int matrix[][]={                        {1,2,3},
 							{4,5,6},
 							{7,8,9},
 							{10,11,12}
